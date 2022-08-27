d313f9605a857af61c7089f70c2945c82650d706
saeedorsaid <sabernia.saeed@gmail.com>
2022-08-27 11:39:17 +0000

    feat: create a secret number

--- number_guess.sh
+++ number_guess.sh
@@ -2,15 +2,17 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo -e "Enter your username:\n"
+echo -e "\nEnter your username:"
 read USERNAME
 
-GAMES_PLAYED = $($PSQL "SELECT games_played FROM users WHERE username='$USERNAME")
-BEST_GAME = $($PSQL "SELECT best_game FROM users WHERE username='$USERNAME")
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
 
 if [[ -z $GAMES_PLAYED ]]
 then 
   echo -e "Welcome, $USERNAME! It looks like this is your first time here.\n"
 else
   echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.\n"
-  
+fi
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1))
